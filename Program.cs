ebb43c692fc203cb975a75cbfdd8a97101383003
matijaopacak06 <153612293+matijaopacak06@users.noreply.github.com>
2024-10-30 09:13:56 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq_query_zadatak
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Student collection
+            List<Student> studentList = new List<Student>() {
+                new Student() { StudentID = 1, StudentName = "John" } ,
+                new Student() { StudentID = 2, StudentName = "Moin" } ,
+                new Student() { StudentID = 3, StudentName = "Bill" } ,
+                new Student() { StudentID = 4, StudentName = "Ram" } ,
+                new Student() { StudentID = 5, StudentName = "Ron"  }
+            };
+
+            // upit i izvršavanje upita
+
+            var rez = from s in studentList
+                          where s.StudentName.Contains('o')
+                          select s.StudentName;
+
+            foreach (var student in rez)
+            {
+                Console.WriteLine(student.ToString());
+            }
+            Console.ReadKey();
+        }
+    }
+}
+
+public class Student
+{
+
+    public int StudentID { get; set; }
+    public string StudentName { get; set; }
+
+    public override string ToString()
+    {
+        string ispis = "Redni broj:" + this.StudentID + "Ime" + this.StudentName;
+        return ispis;
+    }
+}
+        
+    
+
